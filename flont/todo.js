318eb1952e579c42f5672d5cdaedc6f42f2f65f2
shimazu27 <vigaku-tehutehu.doa@i.softbank.jp>
2021-01-13 17:21:36 +0900

    todo

--- flont/todo.js
+++ flont/todo.js
@@ -1,3 +1,16 @@
-const addtask = document.getElementById('add');
-const addlist = document.getElementById('addlist');
-const title = docyment.getElementById('title');
+
+const addTask = document.getElementById("addTask");
+const list = document.getElementById("list");
+const taskTitle = document.getElementById("taskTitle");
+
+const addTodo = task => {
+  const taskList = document.createElement("li");
+  taskList.textContent = task;
+  list.appendChild(taskList);
+};
+
+addTask.addEventListener("click",evnet => {
+  const task = taskTitle.value;
+  addTodo(task);
+  taskTitle.value = '';
+});
