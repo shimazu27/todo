7bbe0f6fd70b42a8e1e2a7727b2d0ed10d13d025
shimazu27 <vigaku-tehutehu.doa@i.softbank.jp>
2021-01-15 15:08:39 +0900

    todo

--- flont/todo.js
+++ flont/todo.js
@@ -5,11 +5,21 @@ const taskTitle = document.getElementById("taskTitle");
 
 const addTodo = task => {
   const taskList = document.createElement("li");
+  const removeButton = document.createElement("button");
+
   taskList.textContent = task;
+
   list.appendChild(taskList);
+  taskList.appendChild(removeButton);
+
+  removeButton.textContent = '削除';
+  removeButton.addEventListener("click", () => {
+    const rmtask = removeButton.closest("li");
+    list.removeChild(rmtask);
+ });
 };
 
-addTask.addEventListener("click",evnet => {
+addTask.addEventListener("click", () => {
   const task = taskTitle.value;
   addTodo(task);
   taskTitle.value = '';
